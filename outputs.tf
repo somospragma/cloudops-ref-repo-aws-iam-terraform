4a3478550e62d138e4c1e54969f1022b0c880bab
farciniegas52 <felipe.arciniegas@pragma.com.co>
2025-01-17 14:20:24 -0500

    Update providers

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "iam_role_info" {
-  value = {for role in aws_iam_role.iam_role : role.tags_all.application_id => {"role_arn" : role.arn, "role_name" : role.tags_all.Name}}
-}
+# output "iam_role_info" {
+#   value = {for role in aws_iam_role.iam_role : role.tags_all.application_id => {"role_arn" : role.arn, "role_name" : role.tags_all.Name}}
+# }
