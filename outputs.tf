9d0e4caee635af07587cf710dd32dbaca3441af0
farciniegas52 <felipe.arciniegas@pragma.com.co>
2025-01-17 22:08:46 -0500

    Actualizacion Service

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,10 @@
-# output "iam_role_info" {
-#   value = {for role in aws_iam_role.iam_role : role.tags_all.application_id => {"role_arn" : role.arn, "role_name" : role.tags_all.Name}}
-# }
+output "iam_role_info" {
+  description = "Map of IAM role information containing ARN, name and tags"
+  value = {
+    for functionality_app_service, role in aws_iam_role.iam_role : functionality_app_service => {
+      arn = role.arn
+      name = role.name
+      tags = role.tags
+    }
+  }
+}
